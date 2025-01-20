1977cce2d82897c0afaf1bac203e8d01d60d9908
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-01-20 00:30:37 -0500

    Add files via upload

--- inc/tomc-bookshelves-route.php
+++ inc/tomc-bookshelves-route.php
@@ -32,7 +32,7 @@ function deleteShelfProduct($data){
     if (is_user_logged_in()){
         $query = 'delete from %i where productid = %d and bookshelfid = %d';
         $deletion = $wpdb->get_results($wpdb->prepare($query, $bookshelf_products_table, $productId, $shelfId), ARRAY_A);
-        return 'success';
+        return $wpdb->prepare($query, $bookshelf_products_table, $productId, $shelfId);
     } else {
         wp_safe_redirect(site_url('/my-account'));
         return 'fail';
